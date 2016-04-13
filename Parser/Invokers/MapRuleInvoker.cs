c0eacdd4bb9e8fca0abec145ebd2cf3e411ca401
barbarossia <barbarossia.gol@gmail.com>
2016-04-13 09:53:49 +0800

    add

---
+++ Parser/Invokers/MapRuleInvoker.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MapReduce.Parser.Invokers {
+    public class MapRuleInvoker<T, TResult> : RuleInvokerBase {
+        public MapRuleInvoker(Type ruleType) : base(ruleType) {
+        }
+
+        public override LambdaExpression Invoke() {
+            var rule = (IMapRule<T, TResult>)RuleType.CreateInstance();
+            var para1 = Expression.Parameter(typeof(T));
+            Expression<Func<T, IEnumerable<TResult>>> n = (t) => rule.Execute(t);
+            return Expression.Lambda<Func<T, IEnumerable<TResult>>>(Expression.Invoke(n, para1), para1);
+        }
+    }
+}
---
+++ Parser/Invokers/RuleGroupInvoker.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MapReduce.Parser.Invokers {
+    public class RuleGroupInvoker<T> : GroupInvokerBase {
+        public RuleGroupInvoker(params LambdaExpression[] exprs) : base(exprs) {
+        }
+
+        public override LambdaExpression Invoke() {
+            var converted = Exprs.Select(e => (Expression<Func<T, T>>)e);
+            var result = converted.Aggregate((curr, next) => curr.Concat(next));
+            return result;
+        }
+    }
+}
